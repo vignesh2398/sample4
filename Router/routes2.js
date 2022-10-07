098af7be35ae78234859a769b05987386fc09eb1
vignesh2398 <rvignesh60474@gmail.com>
2022-10-08 00:08:46 +0530

    first commit

--- Router/routes2.js
+++ Router/routes2.js
@@ -1,6 +1,7 @@
+const express = require('express');
 const { verifys } = require("../Function/Verify");
 const { User } = require("../Models/User");
-const express = require('express');
+const { Repo } = require("../Models/Reposchema");
 const router =express.Router();
 
 router.get('/allrepo',verifys,async(req,res)=>{
@@ -18,6 +19,37 @@ router.get('/allrepo',verifys,async(req,res)=>{
       res.send(e)
     }
   })
+
+  router.put('/commit',verifys,async(req,res)=>{
+    try {
+      //const email=await req.header("email");
+      const id= req.header('id');
+     // const user = await User.findOne({email:email}).populate("repo","_id")
+      const commita= await Repo.findByIdAndUpdate({_id:id},{$push:{"description":{description:req.body.description,commit:req.body.commit}}},{new:true})
+      
+      res.status(200).send({success:true,data:commita})
+    } catch (error) {
+      
+      res.send(error)
+    }
+  })
+
+  router.delete('/delete/:id',verifys,async(req,res)=>{
+    try {
+      id=req.params.id
+      
+      const user= await Repo.findById({_id:id})
+     const del= await User.findByIdAndUpdate({_id:user.RepoOwner},{$pull:{"repo":id}},{new:true})
+  
+     const data= await Repo.findByIdAndDelete({_id:id})
+      res.send({message:"user delete",success:true})
+    } catch (error) {
+      
+      res.send({message:"User Doesnt exist",success:false})
+    }
+  })
+
+
 
   module.exports=router
   
