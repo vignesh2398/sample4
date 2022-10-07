597a2ecc45b236cdd7ab66ad7b42f1fe12da0c27
vignesh2398 <rvignesh60474@gmail.com>
2022-10-07 23:53:32 +0530

    first commit

--- Router/routes.js
+++ Router/routes.js
@@ -112,21 +112,7 @@ router.post('/repo',verifys,async(req,res)=>{
    
    })
 
-   router.get('/allrepo',verifys,async(req,res)=>{
-    try {
-      const email=req.header("email");
-     
-      const user = await User.findOne({email:email}).populate("repo")
-      //const user = await User.find().populate("repo")
-      //console.log(user)
-      
-      //res.status(200).send(user[0].repo[0]._id)
-      res.status(200).send(user)
-    } catch (e) {
-          
-      res.send(e)
-    }
-  })   
+ 
 
 
 module.exports = router;
